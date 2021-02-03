c5ac649179c563d1f6ae822e87d2e6fae27cf650
hyrodium <hyrodium@gmail.com>
2021-02-04 02:12:59 +0900

    update code to generate one tooth

--- tooth.jl
+++ tooth.jl
@@ -3,10 +3,25 @@ using Luxor
 function involute(t)
     x = cos(t)+t*sin(t)
     y = sin(t)-t*cos(t)
-    return (x,y)
+    return [x,y]
 end
 
-point(x,y;ul=50) = Luxor.Point(ul*x,-ul*y)
+function point(x,y;ul=50)
+    return Luxor.Point(ul*x,-ul*y)
+end
+
+f(t) = sin(t)-t*cos(t)
+g(t) = t*sin(t)
+newton(t) = t-f(t)/g(t)
+
+t0 = 4.5
+const T = newton(newton(newton(newton(t0))))
+
+N = 48
+ps = [involute(T*i/N) for i in 0:N]
+ps[end] = [ps[end][1],0.0]
+append!(ps,[[1,-1].*ps[i] for i in N:-1:2])
+
 
 begin
     Drawing(1000, 1000, "hello-world.png")
@@ -15,11 +30,11 @@ begin
     sethue("blue")
     circle(O,50,:stroke)
     sethue("red")
-    for t in 0:0.1:4.2
-        line(point(involute(t)...),point(involute(t+0.1)...),:stroke)
+    for i in 1:2N-1
+        line(point(ps[i]...),point(ps[i+1]...),:stroke)
     end
+    line(point(ps[2N]...),point(ps[1]...),:stroke)
     fontsize(50)
     finish()
     preview()
 end
-
